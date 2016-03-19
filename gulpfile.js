a6f9a38deb1351779cb7252221c26be301f9be8b
shigemk2 <i.am.shige@gmail.com>
2016-03-19 16:23:59 +0900

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require("vinyl-source-stream");
+var reactify = require('reactify');
+
+gulp.task('browserify', function(){
+  var b = browserify({
+    entries: ['./src/example.js'],
+    transform: [reactify]
+  });
+  return b.bundle()
+    .pipe(source('example.js'))
+    .pipe(gulp.dest('./dist'));
+});
